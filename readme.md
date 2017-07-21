8fafee34fd2d293533b8f37f8edc50c8b9264b48
Oskar Thornblad <oskar.thornblad@gmail.com>
2017-07-21 19:37:56 +0200

    Removed most of the features, but it compiles

--- readme.md
+++ readme.md
@@ -1,11 +1,3 @@
-# What is it?
-
-A really crappy [GitVersion](https://github.com/GitTools/GitVersion) made in Rust. If you use this in production, you're nuts.
-
-# What's it for?
-
-Tagging up releases automatically on CI servers.
-
 # Building on a Mac
 
 The `git2` crate needs `cmake` (it's on brew), and I had to supply the builder with `OPENSSL_ROOT_DIR` or it wouldn't compile [libgit2](https://github.com/libgit2/libgit2) Correctly:
@@ -17,29 +9,9 @@ OPENSSL_ROOT_DIR=/usr/local/opt/openssl cargo build
 # Usage
 
 ````sh
-next_version # In a git repo, defaults to current path
-next_version /path/to/repo # Repo path
-next_version --major-regexp "_major_" # Match "_major_" against commit messages for major bumps
-next_version --minor-regexp "_feature_" # Match "_feature_" against commit messages for minor bumps
-next_version --patch-regexp "_fix_" # Match "_fix_" against commit messages for patch bumps
+next_version # Shows current latest semver version or 0.0.1
+next_version --major # Extracts latest and bumps major
+next_version --minor # Extracts latest and bumps minor
+next_version --patch # Extracts latest and bumps patch
+next_version --patch ../foo # Specify git repo in another directory
 ````
-
-# Rules
-
-1. Finds highest current semver version tag.
-2. Checks all commit messages from that point to look for bump messages.
-3. If none were found, metadata increments a build number.
-4. Prints out the suggested name for the next version's tag.
-
-* If the repo lacks semver tags completely, the next suggested version will be `0.0.1`.
-* If the current version is `1.2.3`, and a commit message matching a minor bump is detected, the next suggested version will be `1.3.0`.
-* If the current version is `1.3.0`, and no bumps are detected, the next suggested version will be `1.3.0+1`.
-* If the current version is `1.3.0+1`, and no bumps are detected, the next suggested version will be `1.3.0+2`.
-* If the current version is `1.3.0+2`, and a patch bump is detected, the next suggested version will be `1.3.1`.
-* If the current version is `1.3.1` and _no commit messages are found after that version_, the command will exit with a non-zero status.
-
-## Default bump match patterns
-
-* `:major:` for major bumps
-* `:minor:` for minor bumps
-* `:patch:` for patch bumps
--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,11 @@
 extern crate semver;
 extern crate git2;
-extern crate regex;
-extern crate getopts;
+extern crate clap;
 
-use std::process;
 use semver::Version;
 use git2::Repository;
-use git2::SORT_TOPOLOGICAL;
-use git2::SORT_TIME;
-use git2::SORT_REVERSE;
-use regex::Regex;
 use std::io::{self, Write};
-use std::env;
-use getopts::Options;
-
-/// Extracts build number metadata from a version if available and increments it
-fn get_metadata(version: &Version) -> u64 {
-    let mut build: u64;
-
-    if version.build.len() == 1 {
-        let build_id = &version.build[0];
-        build = build_id.to_string().parse::<u64>().unwrap();
-        build = build + 1;
-    } else {
-        build = 1;
-    }
-
-    return build;
-}
-
-#[derive(Debug)]
-struct Bumps {
-    objects: u64,
-    major: bool,
-    minor: bool,
-    patch: bool
-}
-
-/// Walk the repository from the given version up to HEAD and find all commit messages with bumps
-fn detect_bumps(repo: &Repository, start_version: &Version, major_pattern: &str, minor_pattern: &str, patch_pattern: &str) -> Bumps {
-    // Extract the version's object
-    let object = repo.revparse_single(&start_version.to_string()).unwrap();
-    let sha = object.id();
-
-    // Set up the revwalker
-    let mut revwalker = repo.revwalk().unwrap();
-    // Sort chronically backwards
-    revwalker.set_sorting(SORT_TOPOLOGICAL | SORT_TIME | SORT_REVERSE);
-    // Start at HEAD
-    revwalker.push_head().unwrap();
-
-    // Hide the version's object and everything older
-    revwalker.hide(sha).unwrap();
-
-    // Walk through all of the objects and detect the bumps
-    let mut bumps = Bumps {
-        objects: 0,
-        major: false,
-        minor: false,
-        patch: false
-    };
-
-    // Set up reg-expes
-    let major_re = Regex::new(major_pattern).unwrap();
-    let minor_re = Regex::new(minor_pattern).unwrap();
-    let patch_re = Regex::new(patch_pattern).unwrap();
-
-    for sha in revwalker {
-        // Count objects
-        bumps.objects += 1;
-        // Find object
-        let sha_str = sha.to_string();
-        let object = repo.revparse_single(&sha_str).unwrap();
-        // Read as commit if possible
-        match object.as_commit() {
-            Some(commit) => {
-                // Read commit message
-                let message = commit.message().unwrap();
-
-                // Match against our regular expressions
-                if major_re.is_match(message) {
-                    bumps.major = true;
-                } else if minor_re.is_match(message) {
-                    bumps.minor = true;
-                } else if patch_re.is_match(message) {
-                    bumps.patch = true;
-                }
-            }
-            None => {}
-        }
-    }
-
-    return bumps;
-}
+use clap::{Arg, App};
 
 /// Get the highest version from the repo tags
 fn get_highest_version(repo: &Repository) -> Option<Version> {
@@ -125,115 +38,53 @@ fn get_highest_version(repo: &Repository) -> Option<Version> {
     }
 }
 
-/// Help printout using getopts
-fn print_usage(executable_path: &str, opts: Options) {
-    let brief = format!("Usage: {} [REPOSITORY] [options]", executable_path);
-    print!("{}", opts.usage(&brief));
-}
-
 fn main() {
-    // Handle arguments
-    let args: Vec<String> = env::args().collect();
-    // Extract the executable's path
-    let executable_path = args[0].clone();
+    let matches = App::new("next_version")
+        .version("2.0")
+        .about("Helps guess the next git semver tag")
+        .author("oskar.thornblad@gmail.com")
+        .arg(
+            Arg::with_name("repo_dir")
+                .index(1)
+                .short("r")
+                .long("repo")
+                .value_name("DIR")
+                .help("Git repository")
+                .takes_value(true)
+        )
+        .arg(Arg::with_name("major").short("M").long("major").help("Bump major version"))
+        .arg(Arg::with_name("minor").short("m").long("minor").help("Bump minor version"))
+        .arg(Arg::with_name("patch").short("p").long("patch").help("Bump patch version"))
+        .get_matches();
 
-    // Set up the options
-    let mut opts = Options::new();
-    opts.optopt("m", "major-regexp", "set major regexp pattern", "PATTERN");
-    opts.optopt("f", "minor-regexp", "set minor regexp pattern", "PATTERN");
-    opts.optopt("p", "patch-regexp", "set patch regexp pattern", "PATTERN");
-    // Help flag
-    opts.optflag("h", "help", "print this help menu");
+    let repo_dir = matches.value_of("repo_dir").unwrap_or(".");
 
-    // Parse the arguments, crash on illegal arguments
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => { m }
-        Err(f) => { panic!(f.to_string()) }
-    };
+    let repo = Repository::open(repo_dir).expect("Couldn't open git repository");
 
-    // Print help
-    if matches.opt_present("h") {
-        print_usage(&executable_path, opts);
-        return;
-    }
-
-    // Major regular expressions
-    let major_regexp = match matches.opt_str("m") {
-        Some(r) => r,
-        None => r":major:".to_string()
-    };
-    // Minor regular expressions
-    let minor_regexp = match matches.opt_str("m") {
-        Some(r) => r,
-        None => r":minor:".to_string()
-    };
-    // Patch regular expressions
-    let patch_regexp = match matches.opt_str("m") {
-        Some(r) => r,
-        None => r":patch:".to_string()
-    };
-
-    // Repository directory
-    let repo_dir = if !matches.free.is_empty() {
-        matches.free[0].clone()
-    } else {
-        ".".to_string()
-    };
-
-    // Open the repo
-    let repo = Repository::open(repo_dir).unwrap();
-    // Retrieve the highest version
+    // Extract repo's highest version
     let highest_version = match get_highest_version(&repo) {
-        // Version found
         Some(version) => version,
-        // Fallback to 0.0.1
         None => Version {
             major: 0,
             minor: 0,
             patch: 1,
             pre: vec!(),
-            build: vec!()
+            build: vec!(),
         }
     };
 
-    // Detect commit message bumps
-    let bumps = detect_bumps(&repo, &highest_version, &major_regexp, &minor_regexp, &patch_regexp);
-    // If no objects were walked, we don't want to tag the release at all
-    if bumps.objects == 0 {
-        process::exit(1);
-    }
-
-    // Clone to a new version
+    // Construct the new version
     let mut new_version = highest_version.clone();
 
-    // Perform bumps
-    if bumps.patch {
+    if matches.is_present("major") {
+        new_version.increment_major();
+    } else if matches.is_present("minor") {
+        new_version.increment_minor();
+    } else if matches.is_present("patch") {
         new_version.increment_patch();
     }
-    if bumps.minor {
-        new_version.increment_minor();
-    }
-    if bumps.major {
-        new_version.increment_major();
-    }
 
-    // Handle build metadata
-    let build;
-    if !bumps.patch && !bumps.minor && !bumps.major {
-        build = get_metadata(&new_version);
-    } else {
-        build = 0;
-    }
+    let output_version = format!("{}.{}.{}", new_version.major, new_version.minor, new_version.patch);
 
-    // Construct a version name
-    let output_version;
-    if build == 0 {
-        // If build metadata was 0, do not include it in the version name
-        output_version = format!("{}.{}.{}", new_version.major, new_version.minor, new_version.patch);
-    } else {
-        output_version = format!("{}.{}.{}+{}", new_version.major, new_version.minor, new_version.patch, build);
-    }
-
-    // Send to stdout
     io::stdout().write(output_version.as_bytes()).unwrap();
 }
